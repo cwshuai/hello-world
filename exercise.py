a2442004fe14ac5027663c5ed0e4864fb015ff19
wushuai.chang <wushuai.chang@ashermed.com>
2021-12-09 14:53:40 +0800

    first commit

---
+++ exercise.py
@@ -0,0 +1,46 @@
+# str1 ="have a  nice day"
+# str2 ='have a  nice day'
+# print(str1 == str2)
+
+# first_string = 'A literal string'
+# second_string = "A literal string"
+# print(second_string == first_string)
+
+
+# first_string = 'A literal " string'
+# second_string = "A literal ' string"
+# print(first_string)
+# print(second_string)
+
+
+# first_string = 'A literal \' string'
+# second_string = "A literal \" string"
+# second111_string = "A literal string with a \n new line character"
+# second222_string = "A literal string with a \t tab character"
+# print(first_string)
+# print(second_string)
+# print(second111_string)
+# print(second222_string)
+
+
+# second111_string = r"A literal string with a \n new line character"
+# print(second111_string)
+
+# tenth_string = '''A literal string
+# on more than one line
+# sometimes known as a verbatim string'''
+
+# eleventh_string = """Another literal string
+#      on more than one line
+# using double quotes"""
+
+# print(tenth_string)
+# print(eleventh_string)
+
+# first = 'Conrad'
+# second = 'Grant'
+# third = 'Bob'
+# print(first, second)
+# print(first, second, third)
+# print(first, second, third, sep='-')
+# print(first, second, third, sep='-', end='.')
---
+++ hello.py
@@ -0,0 +1,69 @@
+# print("hello world")
+# print("hello world")
+# print("What is your name?")
+# name = input()
+# print("hello," + name)
+
+
+# print("first number:")
+# first_n= input()
+# print("second number:")
+# second_n = input()
+# sum = first_n + second_n
+# print(sum)
+
+
+# print("first number:")
+# first_n= int(input())
+# print("second number:")
+# second_n = int(input())
+# sum = first_n + second_n
+# print(sum)
+
+
+# print("first number:")
+# first_n= int(input())
+# print("second number:")
+# second_n = int(input())
+# sum = first_n + second_n
+# print("sum" +sum)
+
+# print("first number:")
+# first_n= int(input())
+# print("second number:")
+# second_n = int(input())
+# sum = first_n + second_n
+# print("sum: " + str(sum))
+
+
+# 当前日期（任何格式）
+# 早餐摄入的卡路里数
+# 午餐摄入的卡路里数
+# 晚餐摄入的卡路里数
+# 通过零食摄入的卡路里数
+
+# Today's date?
+# Thursday
+# Breakfast calories?
+# 100
+# Lunch calories?
+# 200
+# Dinner calories?
+# 300
+# Snack calories?
+# 400
+# Calorie content for Thursday: 1000
+
+print("Today's date?")
+date =input()
+print("Breakfast calories?")
+b = int(input())
+print("Lunch calories?")
+l =int(input())
+print("Dinner calories?")
+d =int(input())
+print("Snack calories?")
+s =int(input())
+all = b + l + d+ s
+print("Calorie content for " + str(date) + ": "+ str(all))
+
